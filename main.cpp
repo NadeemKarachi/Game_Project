4ee03b7090f35c565301ea8c4c0323d8f6593a2a
Imad Mashhood <mashhoodimad@gmail.com>
2022-04-29 00:46:44 -0500

    Refactored main

--- main.cpp
+++ main.cpp
@@ -8,16 +8,16 @@
 #include "Human.h"
 #include "game.h"
 #include "Elf.h"
+
 Dwarf createDwarf(bool isGenerated);
 Elf createElf(bool isGenerated);
+Human createHuman(bool isGenerated);
 
 using namespace std;
 
 int main(){
     Character vChar;
     Game vGame;
-    Human vHum;
-    //Elf vElf;
 
     string name, occupation, race, weapon;
     int stamina;
@@ -31,19 +31,25 @@ int main(){
     cout << "TO START PLAYING PLEASE SELECT A CHARACTER" << endl;
 
     //Input Validation
-    for(int i = 0; i < 3; i++){
-        while (true) {
-            cout << "1. Dwarf" << endl << "2. Human" << endl << "3. Elf" << endl;
-            cin >> characterChoice;
-            if (characterChoice > 3 || characterChoice < 1) {
-                cout << "Invalid selection! Please Try Again" << endl;
-                //Clears prev input if invalid
-                cin.clear();
-                cin.ignore(1000000, '\n');
-            } else
-                break;
-        }
+    while (true) {
+        cout << "1. Dwarf" << endl << "2. Human" << endl << "3. Elf" << endl;
+        cin >> characterChoice;
+        if (characterChoice > 3 || characterChoice < 1) {
+            cout << "Invalid selection! Please Try Again" << endl;
+            //Clears prev input if invalid
+            cin.clear();
+            cin.ignore(1000000, '\n');
+        } else
+            break;
+    }
 
+    if (characterChoice == 1) {
+        Dwarf vDwarf = createDwarf(false);
+        //Dwarf *d = new Dwarf(name, race, (level), (health), (stamina), weapon);
+    } else if (characterChoice == 2) {
+        Human vHum = createHuman(false);
+        //Human *h = new Human(name, race, (level), (health), (stamina), weapon);
+    } else if (characterChoice == 3) {
         if (characterChoice == 1) {
             Dwarf vDwarf = createDwarf(false);
             /*string weapon_name;
@@ -100,8 +106,6 @@ int main(){
             i++;*/
         }
         cout << endl;
-        if(i<3){
-            cout <<"PLEASE SELECT YOUR OPPONENT: "<<endl;}
     }
 
 
@@ -148,10 +152,10 @@ Dwarf createDwarf(bool isGenerated){
     string charName;
     int weaponChoice;
 
-    if(isGenerated)
+    if (isGenerated)
         charName = "Enemy Dwarf";
 
-    else{
+    else {
         cout << "You have selected Dwarf!" << endl;
         cout << "GIVE A NAME TO THE DWARF!" << endl;
         cin >> charName;
@@ -163,9 +167,9 @@ Dwarf createDwarf(bool isGenerated){
     vDwarf.SetLevel(80);
 
     while (true) {
-        if(isGenerated)
+        if (isGenerated)
             weaponChoice = rand() % 4;
-        else{
+        else {
             cout << "Please select a weapon for " << charName << endl;
             cout << "1. Sword" << endl << "2. Axe" << endl << "3. Hammer" << endl;
             cin >> weaponChoice;
@@ -188,6 +192,48 @@ Dwarf createDwarf(bool isGenerated){
     vDwarf.Print();
 }
 
+Human createHuman(bool isGenerated) {
+    Human vHum;
+    string charName;
+    int weaponChoice;
+    if (isGenerated)
+        charName = "Enemy Human";
+    else {
+        cout << "You have selected Human!" << endl;
+        cout << "NAME YOUR HUMAN!" << endl;
+        cin >> charName;
+    }
+    vHum.SetName(charName);
+    vHum.SetRace("Human");
+    vHum.SetHealth(90);
+    vHum.SetLevel(90);
+
+    while (true) {
+        if (isGenerated)
+            weaponChoice = rand() % 4;
+        else {
+            cout << "Please select a weapon for " << charName << endl;
+            cout << "1. Sword" << endl << "2. Kyoketsu-Shoge" << endl << "3. Flail" << endl;
+            cin >> weaponChoice;
+        }
+        if (weaponChoice == 1) {
+            vHum.SetWeapon("Sword");
+            break;
+        } else if (weaponChoice == 2) {
+            vHum.SetWeapon("Kyoketsu-Shoge");
+            break;
+        } else if (weaponChoice == 3) {
+            vHum.SetWeapon("Flail");
+            break;
+        } else {
+            cout << "No weapon has been selected!";
+            cin.clear();
+            cin.ignore(1000000, '\n');
+        }
+    }
+    vHum.Print();
+}
+
 Elf createElf(bool isGenerated){
     Elf vElf;
     string charName;
@@ -199,16 +245,18 @@ Elf createElf(bool isGenerated){
         cout << "GIVE A NAME TO THE ELF!" << endl;
         cin >> charName;
     }
-
     vElf.SetName(charName);
     vElf.SetRace("Elf");
     vElf.SetHealth(95);
     vElf.SetLevel(85);
-
     while (true) {
-        cout << "Please select a weapon for " << charName << endl;
-        cout << "1. Bow&Arrow" << endl << "2. Sword" << endl << "3. Javelin" << endl;
-        cin >> weaponChoice;
+        if(isGenerated)
+            weaponChoice = rand() % 4;
+        else{
+            cout << "Please select a weapon for " << charName << endl;
+            cout << "1. Bow&Arrow" << endl << "2. Sword" << endl << "3. Javelin" << endl;
+            cin >> weaponChoice;
+        }
         if (weaponChoice == 1) {
             vElf.SetWeapon("Bow&Arrow");
             break;
